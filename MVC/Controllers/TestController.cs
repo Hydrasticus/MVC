9a5234a2e8cd8b383ce0c6a68249a8ae493f676a
Alo Pullmann <alpull@ttu.ee>
2018-05-14 22:02:16 +0300

    Lab 2 completed.

--- MVC/Controllers/TestController.cs
+++ MVC/Controllers/TestController.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MVC.Controllers {
+    
     public class TestController : Controller {
+        
         public string GetString() {
             return "Hello world!";
+        }
+
+        public ActionResult GetView() {
+            return View("MyView");
         }
     }
 }
