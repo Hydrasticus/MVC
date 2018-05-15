93f643696a9f1e0ad0a9cae36e1fe6cc5aa97fcf
Alo Pullmann <alpull@ttu.ee>
2018-05-15 15:13:05 +0300

    Lab 14 completed.

--- Core/Employee.cs
+++ Core/Employee.cs
@@ -1,11 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Core {
+﻿namespace Core {
 
     public class Employee {
-
-        private const string requiredField = "Required field!";
-        private const string lengthIsTooBig = "Length should be less than 20 characters!";
 
         public Employee() { }
 
@@ -17,11 +12,10 @@ namespace Core {
 
         public int EmployeeId { get; set; }
         
-        [Required(ErrorMessage = requiredField)]
-        [StringLength(20, ErrorMessage = lengthIsTooBig)]
+        [NameValidation]
         public string FirstName { get; set; }
         
-        [StringLength(20, ErrorMessage = lengthIsTooBig)]
+        [NameValidation]
         public string LastName { get; set; }
         
         public int Salary { get; set; }
